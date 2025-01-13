7485504ebb0005617841ee8d348f8ab9a99e2feb
Senay1675 <senay_93@hotmail.com>
2025-01-13 08:32:49 +0100

    gjort om stylingen på home sidan

--- my-app/src/Pages/HomePage.jsx
+++ my-app/src/Pages/HomePage.jsx
@@ -1,27 +1,61 @@
+// import { Link } from 'react-router-dom';
+// import CardComponent from '../Components/Cardcomponent';
+
+// const HomePage = ({ cards }) => {
+//     return (
+//         <div style={{ display: 'flex', gap: '2rem' }}>
+//             <h1>Active Card</h1>
+//             {cards.filter(card => card.isActive).map(card => (
+//                 <div key={card.cardNumber}>
+//                     <CardComponent card={card} /> 
+//                 </div>
+//             ))}
+
+//             <h1>Inactive Cards</h1>
+//             {cards.filter(card => !card.isActive).map(card => (
+//                 <div key={card.cardNumber}>
+//                     <CardComponent card={card} /> 
+//                     <Link to={`/card/${card.cardNumber}`}>
+//                         View/Edit Card
+//                     </Link>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
+
+// export default HomePage
+
 import { Link } from 'react-router-dom';
 import CardComponent from '../Components/Cardcomponent';
 
 const HomePage = ({ cards }) => {
     return (
-        <div>
-            <h1>Active Card</h1>
-            {cards.filter(card => card.isActive).map(card => (
-                <div key={card.cardNumber}>
-                    <CardComponent card={card} /> 
-                </div>
-            ))}
+        <div style={{ display: 'flex', gap: '2rem' }}>
+            {/* Active Cards */}
+            <div>
+                <h1>Active Card</h1>
+                {cards.filter(card => card.isActive).map(card => (
+                    <div key={card.cardNumber}>
+                        <CardComponent card={card} /> 
+                    </div>
+                ))}
+            </div>
 
-            <h1>Inactive Cards</h1>
-            {cards.filter(card => !card.isActive).map(card => (
-                <div key={card.cardNumber}>
-                    <CardComponent card={card} /> 
-                    <Link to={`/card/${card.cardNumber}`}>
-                        View/Edit Card
-                    </Link>
-                </div>
-            ))}
+            {/* Inactive Cards */}
+            <div>
+                <h1>Inactive Cards</h1>
+                {cards.filter(card => !card.isActive).map(card => (
+                    <div key={card.cardNumber}>
+                        <CardComponent card={card} /> 
+                        <Link to={`/card/${card.cardNumber}`}>
+                            View/Edit Card
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
 
-export default HomePage
+export default HomePage;
