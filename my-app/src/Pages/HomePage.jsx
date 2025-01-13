93ce0301e309f15071c8986cd8efca7c1f40bd3b
Senay1675 <senay_93@hotmail.com>
2025-01-13 08:33:46 +0100

    .

--- my-app/src/Pages/HomePage.jsx
+++ my-app/src/Pages/HomePage.jsx
@@ -1,30 +1,4 @@
-// import { Link } from 'react-router-dom';
-// import CardComponent from '../Components/Cardcomponent';
 
-// const HomePage = ({ cards }) => {
-//     return (
-//         <div style={{ display: 'flex', gap: '2rem' }}>
-//             <h1>Active Card</h1>
-//             {cards.filter(card => card.isActive).map(card => (
-//                 <div key={card.cardNumber}>
-//                     <CardComponent card={card} /> 
-//                 </div>
-//             ))}
-
-//             <h1>Inactive Cards</h1>
-//             {cards.filter(card => !card.isActive).map(card => (
-//                 <div key={card.cardNumber}>
-//                     <CardComponent card={card} /> 
-//                     <Link to={`/card/${card.cardNumber}`}>
-//                         View/Edit Card
-//                     </Link>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default HomePage
 
 import { Link } from 'react-router-dom';
 import CardComponent from '../Components/Cardcomponent';
